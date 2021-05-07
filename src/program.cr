cafef9bc4ae038ae2a91845c4250dcc76994bf18
Will Leinweber <will@bitfission.com>
2021-05-06 18:52:28 -0700

    Add version and help flags

--- src/program.cr
+++ src/program.cr
@@ -1,3 +1,21 @@
 require "./waitpr"
+require "option_parser"
+
+OptionParser.parse do |parser|
+  parser.banner = "Usage: waitpr [arguments]"
+  parser.on("-v", "--version", "Show the version") do
+    puts "waitpr v#{WaitPR::VERSION}"
+    exit
+  end
+  parser.on("-h", "--help", "Show this help") do
+    puts parser
+    exit
+  end
+  parser.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
+end
 
 WaitPR::Program.new.run
